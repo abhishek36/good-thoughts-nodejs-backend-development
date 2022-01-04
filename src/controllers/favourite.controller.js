bbd483db573bb3407515ff4582d2b0d983c6f02e
abhishek36 <akamble9913@gmail.com>
2022-01-04 17:21:35 +0530

    first commit

---
+++ src/controllers/favourite.controller.js
@@ -0,0 +1,15 @@
+//import Favourite model here
+
+const addOrRemoveFavourite = async (req, res) => {
+  //addOrRemoveFavourite api logic here
+};
+
+const getAllFavourite = async (req, res) => {
+  //getAllFavourite api logic here
+};
+
+const FavouriteController = {
+  addOrRemoveFavourite,
+  getAllFavourite,
+};
+module.exports = FavouriteController;
---
+++ src/controllers/thought.controller.js
@@ -0,0 +1,35 @@
+//import Thought model here
+
+const addThought = async (req, res) => {
+  //addThought api logic here
+};
+
+const getAllThought = async (req, res) => {
+  //getAllThought api logic here
+};
+
+const getThoughtByCategoryId = async (req, res) => {
+  //getThoughtByCategoryId api logic here
+};
+
+const getThoughtById = async (req, res) => {
+  //getThoughtById api logic here
+};
+
+const updateThoughtById = async (req, res) => {
+  //updateThoughtById api logic here
+};
+
+const deleteThoughtById = async (req, res) => {
+  //deleteThoughtById api logic here
+};
+
+const ThoughtController = {
+  addThought,
+  getAllThought,
+  getThoughtByCategoryId,
+  getThoughtById,
+  updateThoughtById,
+  deleteThoughtById,
+};
+module.exports = ThoughtController;
